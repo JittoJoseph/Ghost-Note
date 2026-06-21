8174ad552f0cd577a5ce7a6763ca059bc60a31f8
Jitto Joseph <jitto.joseph67@gmail.com>
2026-06-21 13:26:57 +0530

    add simple health endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,13 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.get("/health", (c) => {
+  return c.json({ status: "ok" });
+});
+
+export default app;
