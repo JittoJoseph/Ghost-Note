a4bc4f84137340cb7e49c164c40e891a6edf3cf6
Jitto Joseph <jitto.joseph67@gmail.com>
2026-06-21 23:45:00 +0530

    add CORS handling

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { Env } from "./env";
 import authRouter from "./routes/auth";
 import userRouter from "./routes/user";
@@ -6,6 +7,19 @@ import linksRouter from "./routes/links";
 import dashboardRouter from "./routes/dashboard";
 
 const app = new Hono<Env>();
+
+app.use("/*", async (c, next) => {
+  const allowedOrigin =
+    c.env.FRONTEND_URL?.replace(/\/$/, "") ?? "http://localhost:3000";
+
+  const corsMiddleware = cors({
+    origin: allowedOrigin,
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  });
+
+  return corsMiddleware(c, next);
+});
 
 app.get("/", (c) => {
   return c.text("GhostNote API");
